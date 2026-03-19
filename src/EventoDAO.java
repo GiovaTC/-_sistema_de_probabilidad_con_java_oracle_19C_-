5f633cad4d9499b0e0de6883cd1e74ef07f13e02
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-19 17:47:28 -0500

    commit - EventoDAO / .

--- src/EventoDAO.java
+++ src/EventoDAO.java
@@ -16,4 +16,39 @@ public class EventoDAO {
             e.printStackTrace();
         }
     }
-}
+
+    public void calcularProbabilidad() {
+        try (Connection con = Conexion.getConnection()) {
+
+            CallableStatement cs = con.prepareCall("{call SP_CALCULAR_PROBABILIDAD}");
+            cs.execute();
+
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String[]> listar() {
+        List<String[]> lista = new ArrayList<>();
+
+        try (Connection con = Conexion.getConnection()) {
+
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM EVENTOS");
+
+            while (rs.next()) {
+                lista.add(new String[]{
+                        rs.getString("ID"),
+                        rs.getString("NOMBRE_EVENTO"),
+                        rs.getString("FRECUENCIA"),
+                        rs.getString("PROBABILIDAD")
+                });
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+}   
